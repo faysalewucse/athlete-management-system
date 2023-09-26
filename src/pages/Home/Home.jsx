f617dd898670f62083103625669775e84b641073
riad009 <riadhossinr4@gmai.com>
2023-09-26 10:24:38 +0600

    test

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import { Container } from "../../components/Container";
 export const Home = () => {
   return (
     <div>
-      <Container> This is Home Page</Container>
+      <Container> This is Home Page 2</Container>
     </div>
   );
 };

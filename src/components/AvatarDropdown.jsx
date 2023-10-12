7f86398e9fa852ff462b21e2d743a04cb710be56
faysalewucse <faysal.ewucse@gmail.com>
2023-10-12 09:36:16 +0600

    update profile page with ant modal

---
+++ src/components/AvatarDropdown.jsx
@@ -0,0 +1,24 @@
+import { Dropdown } from "antd";
+import avatar from "/avatar.png";
+
+const AvatarDropdown = ({ items, currentUser }) => {
+  return (
+    <Dropdown placement="bottomRight" menu={{ items }} trigger={["click"]}>
+      <a onClick={(e) => e.preventDefault()}>
+        <img
+          className="border-2 border-primary rounded-full w-10 cursor-pointer"
+          src={
+            currentUser
+              ? currentUser?.photoURL !== ""
+                ? currentUser?.photoURL
+                : avatar
+              : avatar
+          }
+          alt="avatar"
+        />
+      </a>
+    </Dropdown>
+  );
+};
+
+export default AvatarDropdown;

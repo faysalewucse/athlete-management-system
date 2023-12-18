362ea62297b4cf24756229644604e91526467429
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-12-18 17:09:39 +0600

    fetched team

--- src/pages/Dashboard/AddFormModal.jsx
+++ src/pages/Dashboard/AddFormModal.jsx
@@ -1,7 +1,38 @@
-import { Button, Form, Input, Modal, Select } from "antd";
+import { Button, Form, Modal, Select } from "antd";
 import FileUpload from "../../components/FileUpload";
+import { useQuery } from "@tanstack/react-query";
+import { useAuth } from "../../contexts/AuthContext";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useState } from "react";
 
 const AddFormModal = ({ isModalOpen, setIsModalOpen }) => {
+  const { currentUser } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
+  const [teamValue, setTeamValue] = useState("");
+  const {
+    isLoading,
+    data: teams = [],
+    refetch,
+  } = useQuery({
+    queryKey: ["teams", currentUser?.email],
+    queryFn: async () => {
+      let URL = "teams";
+
+      if (currentUser?.role === "admin") {
+        URL = `teams/${currentUser?.email}`;
+      } else if (currentUser?.role === "coach") {
+        URL = `teams/coach-team/${currentUser?.email}`;
+      } else if (currentUser?.role === "athlete") {
+        URL = `teams/athlete-team/${currentUser?.email}`;
+      }
+      const { data } = await axiosSecure.get(`/${URL}`);
+      return data;
+    },
+  });
+
+  const handleTeamChange = (value) => {
+    setTeamValue(value);
+  };
   return (
     <Modal
       width={400}
@@ -13,11 +44,22 @@ const AddFormModal = ({ isModalOpen, setIsModalOpen }) => {
       <Form className="grid grid-cols-1 " layout="vertical">
         <div>
           <Form.Item
-            name="fieldName"
-            label="Field Name:"
-            rules={[{ required: true, message: "Field Name is required" }]}
+            name="team"
+            label="Select Team"
+            rules={[{ required: true, message: "Answer is required" }]}
           >
-            <Input className="w-full px-4 py-2 rounded-lg" size="large" />
+            <Select
+              size="large"
+              className="rounded-lg"
+              onChange={handleTeamChange}
+              placeholder="Select Team"
+            >
+              {teams.map((team) => (
+                <Option key={team?._id} value={team.teamName}>
+                  {team.teamName}
+                </Option>
+              ))}
+            </Select>
           </Form.Item>
         </div>
 
